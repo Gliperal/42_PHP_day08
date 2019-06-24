05b8efab4fea8ae0784d8356172b16f267b22aec
Hiren Patel <hirenpat@e1z3r4p8.42.us.org>
2019-06-23 21:17:03 -0700

    added nav bar

--- game/lobbies.php
+++ game/lobbies.php
@@ -1,70 +1,92 @@
 <html>
 	<head>
 		<title>Lobbies</title>
-		<style>
+			<style>
+				body
+				{
+					background-size: 100% 100%;
+					height: 100%;
+					margin: 0;
+					background-repeat: no-repeat;
+					background-attachment: fixed;
+					background-image: linear-gradient(to bottom, #000066 -2%, #9900cc 101%);
+				}
 
+				div.lobby-open, div.lobby-closed, div.lobby-over, div.new
+				{
+					border: 2px solid black;
+					border-radius: 2px;
+					padding: 5px;
+					text-align: center;
+					font-size: 16pt;
+					font-family: Arial, Helvetica, sans-serif;
+				}
 
-body
-{
-	/* background-image: url("/resources/images/space_background.png"); */
-	background-size: 100% 100%;
-    height: 100%;
-    margin: 0;
-    background-repeat: no-repeat;
-    background-attachment: fixed;
-    background-image: linear-gradient(to bottom, #000066 -2%, #9900cc 101%);
-}
+				div.lobby-open
+				{
+					background: rgba(187, 221, 187, 0.7); #BDB;
+					color: black;
+				}
 
-div.lobby-open, div.lobby-closed, div.lobby-over, div.new
-{
-	border: 2px solid black;
-	border-radius: 2px;
-	padding: 5px;
-	text-align: center;
-	font-size: 16pt;
-	font-family: Arial, Helvetica, sans-serif;
-}
+				div.lobby-closed
+				{
+					background: rgba(221, 221, 187, 0.7); #BDB;
+					color: black;
+				}
 
-div.lobby-open
-{
-	background: rgba(187, 221, 187, 0.7); #BDB;
-	color: black;
-}
+				div.lobby-over
+				{
+					background: rgba(221, 187, 187, 0.7); #BDB;
+					color: black;
+				}
 
-div.lobby-closed
-{
-	background: rgba(221, 221, 187, 0.7); #BDB;
-	color: black;
-}
+				div.new
+				{
+					margin-top: 20px;
+					margin-left: auto;
+					margin-right: auto;
+					max-width: 200px;
+					color: black;
+					background: rgba(136, 136, 221, 0.7); #BDB;
+					background-color: #88D;
+				}
 
-div.lobby-over
-{
-	background: rgba(221, 187, 187, 0.7); #BDB;
-	color: black;
-}
-
-div.new
-{
-	margin-top: 20px;
-	margin-left: auto;
-	margin-right: auto;
-	max-width: 200px;
-	color: black;
-	background: rgba(136, 136, 221, 0.7); #BDB;
-	background-color: #88D;
-}
-
-a.lobby-link
-{
-	text-decoration: none;
-}
-
-
+				a.lobby-link
+				{
+					text-decoration: none;
+				}
+				.topnav {
+				  overflow: hidden;
+				  background-color: rgb(0, 0, 0);
+				}
+				
+				.topnav a {
+				  float: left;
+				  color: #f2f2f2;
+				  text-align: center;
+				  padding: 14px 16px;
+				  text-decoration: none;
+				  font-size: 17px;
+				}
+				
+				.topnav a:hover {
+					background-color: #ddd;
+  					color: black;
+				}
+				
+				.topnav a.active {
+				  background-color: rgb(255, 0, 221);
+				  color: white;
+				}
 		</style>
 	</head>
 	<body>
-
-
+		<div class="topnav">
+			<a href="../homepage.html">Home</a>
+			<a class="active" href="lobbies.php">Lobbies</a>
+			<a href="rankings.php">Ranks</a>
+			<a href="">About</a>
+		</div>
 <?php
 
 include_once("fileData.php");
@@ -88,11 +110,6 @@ foreach ($lobbies as $id=>$lobby)
 }
 
 ?>
-
-		<br>
-		<br>
-		<br>
-		<br>
 		<div style="display: block;margin-left: auto;margin-right: auto">
 			<a class="lobby-link" href="createLobby.php">
 				<img src="../resources/new_lobby_icon.png">
--- game/rankings.php
+++ game/rankings.php
@@ -2,69 +2,95 @@
 	<head>
 		<title>Awesome Battleships Rankings</title>
 		<style>
+				body
+				{
+					background-size: 100% 100%;
+					height: 100%;
+					margin: 0;
+					background-repeat: no-repeat;
+					background-attachment: fixed;
+					background-image: linear-gradient(to bottom, #000066 -2%, #9900cc 101%);
+				}
 
+				table
+				{
+					margin-left: auto;
+					margin-right: auto;
+					margin-top: 42px;
+					min-width: 400px;
+					border-collapse: collapse;
+					text-align: center;
+					font-size: 16pt;
+					font-family: Arial, Helvetica, sans-serif;
+				}
 
-body
-{
-	background-image: url("/resources/images/space_background.png");
-	background-size: 100% 100%;
-}
+				tr:nth-child(even)
+				{
+					background: rgba(221, 221, 255, 0.5);
+				}
 
-table
-{
-	margin-left: auto;
-	margin-right: auto;
-	margin-top: 42px;
-	min-width: 400px;
-	border-collapse: collapse;
-	text-align: center;
-	font-size: 16pt;
-	font-family: Arial, Helvetica, sans-serif;
-}
+				tr:nth-child(odd)
+				{
+					background: rgba(187, 187, 255, 0.5);
+				}
 
-tr:nth-child(even)
-{
-	background: rgba(221, 221, 255, 0.5);
-}
+				td
+				{
+					border: 2px solid black;
+					padding: 5px;
+				}
 
-tr:nth-child(odd)
-{
-	background: rgba(187, 187, 255, 0.5);
-}
+				td.user
+				{
+					border-right: 0;
+					font-weight: bold;
+				}
 
-td
-{
-	border: 2px solid black;
-	padding: 5px;
-}
-
-td.user
-{
-	border-right: 0;
-	font-weight: bold;
-}
-
-td.wins
-{
-	border-left: 0;
-}
-
-
-		</style>
+				td.wins
+				{
+					border-left: 0;
+				}
+				.topnav {
+				  overflow: hidden;
+				  background-color: rgb(0, 0, 0);
+				}
+				
+				.topnav a {
+				  float: left;
+				  color: #f2f2f2;
+				  text-align: center;
+				  padding: 14px 16px;
+				  text-decoration: none;
+				  font-size: 17px;
+				}
+				
+				.topnav a:hover {
+					background-color: #ddd;
+  					color: black;
+				}
+				
+				.topnav a.active {
+				  background-color: rgb(255, 0, 221);
+				  color: white;
+				}
+			</style>
 	</head>
 	<body>
+		<div class="topnav">
+			<a href="../homepage.html">Home</a>
+			<a href="game.php">Lobbies</a>
+			<a class="active"  href="rankings.php">Ranks</a>
+			<a href="">About</a>
+		</div>
+		<?php
+		include_once("stats.php");
+		include_once("errorPage.php");
 
-
-
-<?php
-include_once("stats.php");
-include_once("errorPage.php");
-
-$html = stats_toHTML();
-if (is_array($html))
-	exit(errorPage($html["error"]));
-echo $html;
-?>
+		$html = stats_toHTML();
+		if (is_array($html))
+			exit(errorPage($html["error"]));
+		echo $html;
+		?>
 
 
 
